79f9d3842fd6e2b94aaa20e1602a3826a32afb6e
wolfdale930 <wolfdale930@gmail.com>
2022-12-09 23:26:44 +0530

    added change

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  console.log('Change');
 });
 
 module.exports = app;
